9337c946793173b4a273ee4412d46300c94a24b7
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-24 22:36:10 +0600

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./shared/Footer";
+import Header from "./shared/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+       <main className="container mx-auto max-h-screen">
+         <Header/>
+       {children}
+       <Footer/>
+       </main>
+        
+        </body>
     </html>
   );
 }

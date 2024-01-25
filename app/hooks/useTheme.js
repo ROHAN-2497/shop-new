071fca24c3b080ef2e0401a147fb7f26da846081
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-25 23:55:42 +0600

    up

--- app/hooks/useTheme.js
+++ app/hooks/useTheme.js
@@ -3,6 +3,11 @@ import ThemeContext from "../contexts/ThemeContext";
 
 const useTheme = () => {
   const theme = useContext(ThemeContext);
+  if (!theme) {
+    throw new Error(
+      "you must wrap your application with themeProvider to use the  useTheme"
+    );
+  }
   return theme;
 };
 

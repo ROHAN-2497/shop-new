5cef30081ef3ccccd1607a9f7d7d983521c89a19
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-26 00:43:39 +0600

    navber and footer Complete with full resopnsive

--- app/hooks/useTheme.js
+++ app/hooks/useTheme.js
@@ -3,6 +3,8 @@ import ThemeContext from "../contexts/ThemeContext";
 
 const useTheme = () => {
   const theme = useContext(ThemeContext);
+  const isClient = typeof window === "undefined";
+  if (!isClient && !theme) return {};
   if (!theme) {
     throw new Error(
       "you must wrap your application with themeProvider to use the  useTheme"

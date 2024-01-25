e4dd42e645e9f7fdbe1da3e04e0ee820662524e9
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-25 23:16:08 +0600

    dynamic date for Footer

--- app/shared/Footer.jsx
+++ app/shared/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-  <aside>
-    <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
-  </aside>
-</footer>
+      <aside>
+        <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+      </aside>
+    </footer>
   )
 }
 

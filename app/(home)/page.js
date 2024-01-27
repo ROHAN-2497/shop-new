44c2a5edc2eeb326d2b14870f644e8c0fc139a73
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-27 08:23:29 +0600

    up

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -1,10 +1,5 @@
 import Image from "next/image";
-import HeroSlider from "./HeroSlider";
 
 export default function Home() {
-  return (
-    <main>
-      <HeroSlider />{" "}
-    </main>
-  );
+  return <main>home</main>;
 }

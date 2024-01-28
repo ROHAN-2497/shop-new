27a71efa0cd0169e802077f83c5fe3294b8dee34
Md. Jubayer Ahmed Rohan <rohanislam3669@gmail.com>
2024-01-28 12:56:42 +0600

    create env file for secret file

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,18 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAn4FNMhtgvP59wmtZtJcbPDG4KDVLwDq4",
-  authDomain: "shop-new-bd-512c3.firebaseapp.com",
-  projectId: "shop-new-bd-512c3",
-  storageBucket: "shop-new-bd-512c3.appspot.com",
-  messagingSenderId: "1045537305408",
-  appId: "1:1045537305408:web:73fde8afda929f925280a5",
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;

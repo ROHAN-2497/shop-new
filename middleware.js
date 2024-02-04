47d2c6ec19adb7399019f47c620f103fe94405ce
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-05 05:58:47 +0600

    updated

--- middleware.js
+++ middleware.js
@@ -1,14 +1,21 @@
+import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export const middleware = async (request) => {
   // return NextResponse.redirect(new URL("/home", request.url));
   try {
     let cookies = request.cookies.get("jwt-token")?.value;
-    if (!cookies || cookies.startsWith("Bearer")) throw new Error("Invalid token")
+    if (!cookies || cookies.startsWith("Bearer")) {
+
+      throw new Error("Invalid token")
+    }
+    const secret = new TextEncoder().encode(process.env.jwt_secret);
+    await jwtVerify(jwt, secret)
+    return NextResponse.next();
+
   } catch (error) {
 
   }
-  return NextResponse.next();
 }
 
 export const config = {

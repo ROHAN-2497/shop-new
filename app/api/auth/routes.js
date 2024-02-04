a482767a9a870a6b86d896d4e1ac624421627b6a
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-05 05:17:03 +0600

    api created server site

--- app/api/auth/routes.js
+++ app/api/auth/routes.js
@@ -1,17 +1,25 @@
+const { SignJWT } = require("jose");
+const { cookies } = require("next/headers");
+const { NextResponse } = require("next/server");
+
 const POST = async (request) => {
     const body = await request.json();
-    const secret = new TextEncoder().encode(
-        'cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2',
-    )
+    const secret = new TextEncoder().encode(process.env.jwt_secret)
     const alg = 'HS256'
 
-    const jwt = await new jose.SignJWT({ 'urn:example:claim': true })
+    const jwt = await new SignJWT(body)
         .setProtectedHeader({ alg })
         .setIssuedAt()
-        .setIssuer('urn:example:issuer')
-        .setAudience('urn:example:audience')
-        .setExpirationTime('2h')
+        .setExpirationTime('1000d')
         .sign(secret)
+
+    cookies().set({
+        name: "jwt-token",
+        value: `bearer ${jwt}`,
+        secure: true,
+        httpOnly: true,
+    });
+    return NextResponse.json({nessage: 'token created'})
 
     console.log(jwt)
 }

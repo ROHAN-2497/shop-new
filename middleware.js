a2b1e52f97395c955e58b57ba2547951a3ced306
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-05 05:47:08 +0600

    updated

--- middleware.js
+++ middleware.js
@@ -2,6 +2,12 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   // return NextResponse.redirect(new URL("/home", request.url));
+  try {
+    let cookies = request.cookies.get("jwt-token")?.value;
+    if (!cookies || cookies.startsWith("Bearer")) throw new Error("Invalid token")
+  } catch (error) {
+
+  }
   return NextResponse.next();
 }
 

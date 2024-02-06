825aa6072ba8dff864e67bfa6ecd558e643ee6b5
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-06 23:37:18 +0600

    up

--- src/services/DBConnect.js
+++ src/services/DBConnect.js
@@ -1,6 +1,6 @@
-const DbConnect = () => {
+const DbConnect = async () => {
 
-  
+
     try {
         const uri = "mongodb+srv://xrohaislam373:iPiwswvgIKbY1VRF@cluster0.6j6ktbt.mongodb.net/?retryWrites=true&w=majority";
         // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -11,8 +11,10 @@ const DbConnect = () => {
                 deprecationErrors: true,
             }
         });
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } catch (error) {
-        
+
     }
 
 }

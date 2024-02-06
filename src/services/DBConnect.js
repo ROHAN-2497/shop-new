b8e29d3c55368ad5a97d5e88127f55e6156c1aef
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-06 23:35:29 +0600

    up

--- src/services/DBConnect.js
+++ src/services/DBConnect.js
@@ -1,5 +1,15 @@
-const DbConnect = () =>{
-    
+const DbConnect = () => {
+
+    const uri = "mongodb+srv://xrohaislam373:iPiwswvgIKbY1VRF@cluster0.6j6ktbt.mongodb.net/?retryWrites=true&w=majority";
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
 }
 
 

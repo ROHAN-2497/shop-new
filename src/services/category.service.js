d9c81ec0922a8f86733cbf0d34ed1451a3ebc9a2
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-13 23:22:36 +0600

    up

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,4 +1,6 @@
 import DbConnect from "./DbConnect"
 const getCategoriesFromDb = async () => {
-    const db = await DbConnect
+    const db = await DbConnect();
+    const categoriesCollection = db.collection("categories");
+    return categoriesCollection.find({}).toArray();
 }  

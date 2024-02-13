ed0d0b40def5e54a705ff1b6c9413871db45f32d
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-13 22:45:44 +0600

    up

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import HeroSlider from "./HeroSlider";
+import Categories from "./Categories";
 
 export default function Home() {
   return <main>
     <HeroSlider />
-    
+    <Categories />
   </main>;
 }

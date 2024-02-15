a1f44ae06ace048db84cbbc7259452cf8c42370d
ROHAN-2497 <rohanislam3669@gmail.com>
2024-02-16 03:01:56 +0600

    up

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,8 +1,5 @@
-import "server-only";
-import DbConnect from "./DbConnect";
 
-export const getCategoriesFromDb = async () => {
-  const db = await DbConnect();
-  const categoriesCollection = db.collection("categories");
-  return categoriesCollection.find({}).toArray();
+
+ const getCategoriesFromDb =  () => {
+  
 };
